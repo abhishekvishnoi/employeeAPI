18bcce27a6a70b46ae7230cb4cf85aec88fae27c
abhishek.vishnoi <vishnoi.abhishek@hotmail.com>
2021-11-05 13:05:36 +0530

    first commit.

---
+++ src/main/java/com/learning/employeeapi/Employee.java
@@ -0,0 +1,43 @@
+package com.learning.employeeapi;
+
+public class Employee {
+
+
+    String firstName;
+    String lastName;
+    String organization;
+    String phone;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ src/main/java/com/learning/employeeapi/EmployeeController.java
@@ -0,0 +1,65 @@
+package com.learning.employeeapi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+
+    Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
+    private static List<Employee> employees = new ArrayList<>();
+
+
+    static{
+        Employee emp1 = new Employee() ;
+        emp1.setFirstName("abhishek");
+        emp1.setLastName("vishnoi");
+        emp1.setOrganization("lizard");
+        emp1.setPhone("91-9810-337170");
+
+        Employee emp2 = new Employee() ;
+        emp2.setFirstName("arushi");
+        emp2.setLastName("goyal");
+        emp2.setOrganization("lizard");
+        emp2.setPhone("91-9810-337171");
+
+        employees.add(emp1);
+        employees.add(emp2);
+
+    }
+
+    @GetMapping("/getEmp/{id}")
+    public Employee getEmployee(@PathVariable String id){
+        logger.info("a request received to get employee with id :" + id);
+        Employee emp = new Employee() ;
+        emp.setFirstName("abhishek");
+        emp.setLastName("vishnoi");
+        emp.setOrganization("lizard");
+        emp.setPhone("91-9810-337170");
+
+        return emp;
+    }
+
+
+    @GetMapping("/getEmp")
+    public List<Employee> getEmployees(){
+        logger.info("a request received to get all employees");
+        return employees;
+    }
+
+    @PostMapping("/addEmp")
+    public void addEmployee(@RequestBody Employee emp) {
+        logger.info("a request received to add a new employee");
+        employees.add(emp);
+    }
+
+
+
+
+}
